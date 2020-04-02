f9df58c91185dcb3a100f4385b701b228cd3bf3b
Mathéo Branas <matheo.branas@etu.unilim.fr>
2020-04-02 17:56:24 +0200

    dernier commit

--- src/main/java/fr/unilim/iut/spaceinvaders/SpaceInvaders.java
+++ src/main/java/fr/unilim/iut/spaceinvaders/SpaceInvaders.java
@@ -61,5 +61,14 @@ public void positionnerUnNouveauVaisseau(int x, int y) {
 private boolean estDansEspaceJeu(int x, int y) {
 	return ((x >= 0) && (x < longueur)) && ((y >= 0) && (y < hauteur));
 }
+
+public void deplacerVaisseauVersLaDroite() {
+    if (vaisseau.abscisse()< (longueur-1)) vaisseau.seDeplacerVersLaDroite();
+}
+
+public void deplacerVaisseauVersLaGauche() {
+    if (vaisseau.abscisse()< (longueur-1)) vaisseau.seDeplacerVersLaGauche();
+}
+
 	
 }
